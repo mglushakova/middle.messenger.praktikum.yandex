f69b0e9bddbf83e639407744bb8ae41920e93d70
Marina Glushakova <15359545+mglushakova@users.noreply.github.com>
2026-04-28 15:26:38 +0500

    добавлена верстка формы логина

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,9 @@
 import Handlebars from "handlebars";
 import loginPageTemplate from "./login.hbs?raw";
+import '@/styles/main.scss';
+import '@/styles/pages/login.scss';
+import '@/styles/components/text-input.scss';
+import '@/styles/components/button.scss';
 
 Handlebars.registerPartial("login-page", loginPageTemplate); 
 
