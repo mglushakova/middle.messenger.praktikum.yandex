1eb99d22454e8ffcdd98aa1c38db006c21687cc9
Marina Glushakova <15359545+mglushakova@users.noreply.github.com>
2026-04-28 16:53:43 +0500

    текстовый инпут вынесен в отдельный блок

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -5,7 +5,9 @@ import '@/pages/login/login.scss';
 import '@/blocks/text-input/text-input.scss';
 import '@/blocks/button/button.scss';
 import { initTextInputs } from "../../blocks/text-input/text-input";
+import textInput from "@/blocks/text-input/text-input.hbs?raw";
 
+Handlebars.registerPartial("text-input", textInput);
 Handlebars.registerPartial("login-page", loginPageTemplate); 
 
 const entryNode = document.body; 
