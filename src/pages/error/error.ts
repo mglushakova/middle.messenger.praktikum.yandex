7ad985c7953c0c807e3467820002d4870c5dbd36
Marina Glushakova <15359545+mglushakova@users.noreply.github.com>
2026-04-29 11:23:02 +0500

    добавлена верстка страниц ошибок

--- src/pages/error/error.ts
+++ src/pages/error/error.ts
@@ -1,9 +1,13 @@
-import Handlebars from "handlebars";
-import errorPageTemplate from "./error.hbs?raw";
+import Handlebars from 'handlebars';
+import '@/styles/main.scss';
+import '@/layout/error-page/error-page.scss';
+import errorTemplate from '@/pages/error/error.hbs?raw';
+import errorPageTemplate from '@/layout/error-page/error-page.hbs?raw';
 
+Handlebars.registerPartial("error", errorTemplate); 
 Handlebars.registerPartial("error-page", errorPageTemplate); 
 
 const entryNode = document.body; 
-const compiledTemplate = Handlebars.compile(errorPageTemplate)({});
+const compiledTemplate = Handlebars.compile(errorTemplate)({});
 
 entryNode.innerHTML = compiledTemplate; 
--- src/pages/not-found/not-found.ts
+++ src/pages/not-found/not-found.ts
@@ -1,7 +1,11 @@
-import Handlebars from "handlebars";
-import notFoundPageTemplate from "./not-found.hbs?raw";
+import Handlebars from 'handlebars';
+import '@/styles/main.scss';
+import '@/layout/error-page/error-page.scss';
+import notFoundPageTemplate from './not-found.hbs?raw';
+import errorPageTemplate from '@/layout/error-page/error-page.hbs?raw';
 
 Handlebars.registerPartial("not-found-page", notFoundPageTemplate); 
+Handlebars.registerPartial("error-page", errorPageTemplate); 
 
 const entryNode = document.body; 
 const compiledTemplate = Handlebars.compile(notFoundPageTemplate)({});
