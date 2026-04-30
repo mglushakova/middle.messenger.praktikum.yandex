f1e1015b862809a0d5a50739337171d24a628579
Marina Glushakova <15359545+mglushakova@users.noreply.github.com>
2026-04-30 18:33:22 +0500

    добавлена верстка списка чатов

---
+++ src/pages/chat-page/chat-page.ts
@@ -0,0 +1,18 @@
+import Handlebars from 'handlebars';
+import chatPageTemplate from './chat-page.hbs?raw';
+import '@/styles/main.scss';
+import './chat-page.scss';
+import chatList from '@/blocks/chat-list/chat-list.hbs?raw';
+import '@/blocks/chat-list/chat-list.scss';
+import chatItem from '@/blocks/chat-item/chat-item.hbs?raw';
+import '@/blocks/chat-item/chat-item.scss';
+
+
+Handlebars.registerPartial("chat-page", chatPageTemplate); 
+Handlebars.registerPartial("chat-list", chatList); 
+Handlebars.registerPartial("chat-item", chatItem); 
+
+const entryNode = document.body; 
+const compiledTemplate = Handlebars.compile(chatPageTemplate)({});
+
+entryNode.innerHTML = compiledTemplate; 
