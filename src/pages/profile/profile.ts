d5c3f53e8248953a10b983510dee03ee3dbd42f6
Marina Glushakova <15359545+mglushakova@users.noreply.github.com>
2026-04-30 11:05:10 +0500

    добавлена верстка страницы профиля

---
+++ src/pages/profile/profile.ts
@@ -0,0 +1,19 @@
+import Handlebars from 'handlebars';
+import '@/styles/main.scss';
+import './profile.scss';
+import '@/layout/profile-page/profile-page.scss';
+import profileTemplate from './profile.hbs?raw';
+import profilePageTemplate from '@/layout/profile-page/profile-page.hbs?raw';
+import avatarIcon from '@/blocks/avatar/avatar-placeholder.hbs?raw';
+import profileItem from '@/blocks/profile-item/profile-item.hbs?raw';
+import '@/blocks/profile-item/profile-item.scss';
+
+Handlebars.registerPartial('icon-avatar', avatarIcon);
+Handlebars.registerPartial('profile-page', profilePageTemplate); 
+Handlebars.registerPartial('profile', profileTemplate); 
+Handlebars.registerPartial('profile-item', profileItem); 
+
+const entryNode = document.body; 
+const compiledTemplate = Handlebars.compile(profileTemplate)({});
+
+entryNode.innerHTML = compiledTemplate; 
--- src/pages/user-settings/user-settings.ts
+++
@@ -1,9 +0,0 @@
-import Handlebars from "handlebars";
-import userSettingsPageTemplate from "./user-settings.hbs?raw";
-
-Handlebars.registerPartial("user-settings-page", userSettingsPageTemplate); 
-
-const entryNode = document.body; 
-const compiledTemplate = Handlebars.compile(userSettingsPageTemplate)({});
-
-entryNode.innerHTML = compiledTemplate; 
