edbf55b7a4ddf481c6bcb0c60abc1bf79ca5ec7e
Marina Glushakova <15359545+mglushakova@users.noreply.github.com>
2026-05-01 06:52:27 +0500

    добавлена верстка страницы чата

--- src/pages/chat-page/chat-page.ts
+++ src/pages/chat-page/chat-page.ts
@@ -6,13 +6,21 @@ import chatList from '@/blocks/chat-list/chat-list.hbs?raw';
 import '@/blocks/chat-list/chat-list.scss';
 import chatItem from '@/blocks/chat-item/chat-item.hbs?raw';
 import '@/blocks/chat-item/chat-item.scss';
+import chatWindow from '@/blocks/chat-window/chat-window.hbs?raw';
+import '@/blocks/chat-window/chat-window.scss';
+import messageInput from '@/blocks/message-input/message-input.hbs?raw';
+import '@/blocks/message-input/message-input.scss';
 
 
 Handlebars.registerPartial("chat-page", chatPageTemplate); 
 Handlebars.registerPartial("chat-list", chatList); 
 Handlebars.registerPartial("chat-item", chatItem); 
+Handlebars.registerPartial("chat-window", chatWindow); 
+Handlebars.registerPartial("message-input", messageInput); 
 
 const entryNode = document.body; 
-const compiledTemplate = Handlebars.compile(chatPageTemplate)({});
+const compiledTemplate = Handlebars.compile(chatPageTemplate)({
+    isChatSelected: true
+});
 
 entryNode.innerHTML = compiledTemplate; 
