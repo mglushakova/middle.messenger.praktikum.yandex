86eecf96efb8e83b0642b5fff54e08c541283472
Marina Glushakova <15359545+mglushakova@users.noreply.github.com>
2026-05-10 15:14:13 +0500

    добавлены линтеры

--- README.md
+++ README.md
@@ -15,12 +15,13 @@
 ## 📌 Описание
 
 Список страниц:
-* авторизация
-* регистрация
-* страница чата
-* профиль пользователя и его редактирование
-* 404
-* 500
+
+- авторизация
+- регистрация
+- страница чата
+- профиль пользователя и его редактирование
+- 404
+- 500
 
 [Дизайн](https://www.figma.com/design/jF5fFFzgGOxQeB4CmKWTiE/Chat_external_link?node-id=0-1&p=f&t=snuopyuis4ueaJBC-0)
 
@@ -28,11 +29,11 @@
 
 ## 🛠️ Технологии
 
-* TypeScript
-* Vite
-* Handlebars
-* SCSS (Sass)
-* БЭМ-методология
+- TypeScript
+- Vite
+- Handlebars
+- SCSS (Sass)
+- БЭМ-методология
 
 ---
 
@@ -72,15 +73,15 @@ src/
 
 ## 📖 Доступные страницы:
 
-* `/main.html` — список всех страниц
-* `/login.html` — авторизация
-* `/register.html` — регистрация
-* `/profile.html` — профиль
-* `/profile-edit.html` — редактирование профиля
-* `/password-edit.html` — смена пароля
-* `/chat.html` — страница чата
-* `/error.html` — страница ошибки 404
-* `/not-found.html` — страница ошибки 500
+- `/main.html` — список всех страниц
+- `/login.html` — авторизация
+- `/register.html` — регистрация
+- `/profile.html` — профиль
+- `/profile-edit.html` — редактирование профиля
+- `/password-edit.html` — смена пароля
+- `/chat.html` — страница чата
+- `/error.html` — страница ошибки 404
+- `/not-found.html` — страница ошибки 500
 
 ---
 
--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,23 +1,23 @@
 import Handlebars from "handlebars";
 import registerPageTemplate from "./register.hbs?raw";
-import '@/styles/main.scss';
-import '@/blocks/text-input/text-input.scss';
-import '@/blocks/button/button.scss';
-import formPageTemplate from '@/layout/form-page/form-page.hbs?raw';
-import '@/layout/form-page/form-page.scss';
-import formTemplate from '@/blocks/form/form.hbs?raw';
-import '@/blocks/form/form.scss';
+import "@/styles/main.scss";
+import "@/blocks/text-input/text-input.scss";
+import "@/blocks/button/button.scss";
+import formPageTemplate from "@/layout/form-page/form-page.hbs?raw";
+import "@/layout/form-page/form-page.scss";
+import formTemplate from "@/blocks/form/form.hbs?raw";
+import "@/blocks/form/form.scss";
 import { initTextInputs } from "../../blocks/text-input/text-input";
 import textInput from "@/blocks/text-input/text-input.hbs?raw";
 
 Handlebars.registerPartial("text-input", textInput);
 Handlebars.registerPartial("form-page", formPageTemplate);
 Handlebars.registerPartial("form", formTemplate);
-Handlebars.registerPartial("register-page", registerPageTemplate); 
+Handlebars.registerPartial("register-page", registerPageTemplate);
 
-const entryNode = document.body; 
+const entryNode = document.body;
 const compiledTemplate = Handlebars.compile(registerPageTemplate)({});
 
-entryNode.innerHTML = compiledTemplate; 
+entryNode.innerHTML = compiledTemplate;
 
 initTextInputs(entryNode);
