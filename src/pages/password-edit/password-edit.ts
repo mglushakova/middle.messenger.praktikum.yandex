3f9fc01a9960d73d1086fb02a9b5f9167bd911bd
Marina Glushakova <15359545+mglushakova@users.noreply.github.com>
2026-06-05 14:01:06 +0500

    исправлена ссылка назад на странице профиля

--- src/pages/password-edit/password-edit.ts
+++ src/pages/password-edit/password-edit.ts
@@ -5,7 +5,7 @@ import './password-edit.scss';
 export class PasswordEditPage extends Block<BlockProps> {
   protected template = `<div class="password-edit">
                           {{#> profile-page}}
-                          {{{{ AvatarUpload }}}
+                          {{{ AvatarUpload }}}
                           <div class="password-edit__form">
                             {{{ PasswordEditForm }}}
                           </div>
