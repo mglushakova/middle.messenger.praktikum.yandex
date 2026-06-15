487b34537c6830a021a1eae9eb55596d287164c7
Marina Glushakova <15359545+mglushakova@users.noreply.github.com>
2026-06-15 10:04:53 +0500

    удален vitest из vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 import dotenv from 'dotenv';
 import path from 'path';
 
@@ -18,9 +18,5 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-  },
-
-  test: {
-    environment: 'jsdom',
   },
 });
